f63459004418cc55957b2c1444e3c08e9b82ea7c
JulesZYTB <93528905+JulesZYTB@users.noreply.github.com>
2022-03-19 16:36:47 +0100

    Add files via upload

--- src/commands/reboot.js
+++ src/commands/reboot.js
@@ -1,9 +1,11 @@
+// This may crash your server if you are using Replit.
+
 const Discord = require('discord.js');
 const fetch = require("node-fetch");
 exports.run = (client, message, args) => {
-    if(!global.config.bot.owners.includes(message.author.id)) return  message.reply('could not be granted access permission.')
-	message.channel.send("vCodes: Bot yeniden başlatılıyor.").then(msg => {
-		console.log(`BOT : Yeniden başlatılıyor...`);
+    if(!global.config.bot.owners.includes(message.author.id)) return  message.reply('You dont have permission to use this command')
+	message.channel.send("vCodes: The bot is now rebooting").then(msg => {
+		console.log(`BOT: Restarting from Reboot Command`);
 		process.exit(1);
 	})
 };
@@ -14,6 +16,6 @@ exports.conf = {
 };
 exports.help = {
 	name: 'reboot',
-	description: 'Botu Yeniden Başlatır.',
+	description: 'Reboots the bot',
 	usage: 'reboot'
 };
